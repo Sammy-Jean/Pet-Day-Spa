ac4036268ede12d027a28094545c5afaab39ee9b
Sammy Jean <Sammy.Jean@iCloud.com>
2021-03-14 09:41:52 -0400

    Fixed final changes

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,5 +4,6 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
+
+    mainNav.classList.toggle('active');
 });
