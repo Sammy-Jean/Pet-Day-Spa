99cfb9226ba9fe07eeae8449a7a66e2db03bb5f6
Sammy Jean <Sammy.Jean@iCloud.com>
2021-03-14 09:29:09 -0400

    Added form and fixed styling

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,6 +4,5 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-
-    mainNav.classList.toggle('active');
+  mainNav.classList.toggle('active');
 });
